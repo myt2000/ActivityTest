6c34ff967a9c0fff43ad722c8d68bf74913d85b1
brooks <myt2000@126.com>
2022-09-02 15:35:48 +0800

    title.xml嵌入activity.xml中

--- app/src/main/java/com/brooks/activitytest/TitleLayout.kt
+++ app/src/main/java/com/brooks/activitytest/TitleLayout.kt
@@ -1,14 +1,26 @@
 package com.brooks.activitytest
 
+import android.app.Activity
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.title.view.*
+
 
 class TitleLayout(context: Context, attrs: AttributeSet): LinearLayout(context, attrs){
     init {
         LayoutInflater.from(context).inflate(R.layout.title, this)
+        titleBack.setOnClickListener {
+            val activity = context as Activity  // as 这里做的是类型转换
+            activity.finish()
+        }
+        titleEdit.setOnClickListener {
+            Toast.makeText(context, "You clicked Edit button", Toast.LENGTH_SHORT).show()
+        }
     }
 }
