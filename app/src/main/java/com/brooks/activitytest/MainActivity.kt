9db50918773057880de66587b9a9b980a78126c3
brooks <myt2000@126.com>
2022-09-16 14:44:00 +0800

    删除ActivityMain中的title控件 启动主页面的的时候，添加listView的内容

--- app/src/main/java/com/brooks/activitytest/MainActivity.kt
+++ app/src/main/java/com/brooks/activitytest/MainActivity.kt
@@ -6,15 +6,29 @@ import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val data = listOf("Apple", "Banana","Orange","Watermelon","Pear","Grape",
-        "Pineapple","Strawberry","Cherry","Mango","Apple", "Banana","Orange","Watermelon","Pear","Grape",
-        "Pineapple","Strawberry","Cherry","Mango",)
-    
+//    private val data = listOf("Apple", "Banana","Orange","Watermelon","Pear","Grape",
+//        "Pineapple","Strawberry","Cherry","Mango","Apple", "Banana","Orange","Watermelon","Pear","Grape",
+//        "Pineapple","Strawberry","Cherry","Mango",)
+    private val fruitList = ArrayList<Fruit>()
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        initFruits()    // 初始化数据实例
+        val adapter = FruitAdapter(this, R.layout.fruit_item, fruitList)
         listView.adapter = adapter
+    }
+
+    private fun initFruits(){
+        repeat(2){
+            fruitList.add(Fruit("Apple", R.drawable.apple))
+            fruitList.add(Fruit("Banana", R.drawable.banana))
+            fruitList.add(Fruit("Orange", R.drawable.orange))
+            fruitList.add(Fruit("WaterMelon", R.drawable.watermelon))
+            fruitList.add(Fruit("Grape", R.drawable.grape))
+            fruitList.add(Fruit("Pineapple", R.drawable.pineapple))
+            fruitList.add(Fruit("Strawberry", R.drawable.strawberry))
+        }
     }
 }
