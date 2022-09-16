7aceb7431c0dae4e6351ffa52d7449b6f2bac244
brooks <myt2000@126.com>
2022-09-16 15:52:20 +0800

    ListView添加点击事件， setOnItemClickListener

--- app/src/main/java/com/brooks/activitytest/MainActivity.kt
+++ app/src/main/java/com/brooks/activitytest/MainActivity.kt
@@ -2,22 +2,23 @@ package com.brooks.activitytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-//    private val data = listOf("Apple", "Banana","Orange","Watermelon","Pear","Grape",
-//        "Pineapple","Strawberry","Cherry","Mango","Apple", "Banana","Orange","Watermelon","Pear","Grape",
-//        "Pineapple","Strawberry","Cherry","Mango",)
+
     private val fruitList = ArrayList<Fruit>()
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
         initFruits()    // 初始化数据实例
         val adapter = FruitAdapter(this, R.layout.fruit_item, fruitList)
         listView.adapter = adapter
+        listView.setOnItemClickListener{ parent, view, position,id ->
+            val fruit = fruitList[position]
+            Toast.makeText(this, fruit.name, Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun initFruits(){
