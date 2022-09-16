75fe516d635c7edf8049df339944f6b23b2922b3
brooks <myt2000@126.com>
2022-09-16 15:34:50 +0800

    增加listView的效率，布局通过converview进行缓存，ui控件通过ViewHolder缓存

--- app/src/main/java/com/brooks/activitytest/FruitAdapter.kt
+++ app/src/main/java/com/brooks/activitytest/FruitAdapter.kt
@@ -1,6 +1,5 @@
 package com.brooks.activitytest
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
@@ -11,16 +10,24 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.fruit_item.view.*
 
 class FruitAdapter(activity: Activity, private val resourceId: Int, data: List<Fruit>) : ArrayAdapter<Fruit>(activity, resourceId, data){
+    inner class ViewHolder(val fruitImage: ImageView, val fruitName: TextView)
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = LayoutInflater.from(context).inflate(resourceId, parent, false)
-//        val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
-        val fruitImage: ImageView = view.fruitImage
-//        val fruitName: TextView = view.findViewById(R.id.fruitName)
-        val fruitName: TextView = view.fruitName
+        val view : View
+        val viewHolder: ViewHolder
+        if(convertView == null){
+            view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+            val fruitImage: ImageView = view.fruitImage
+            val fruitName: TextView = view.fruitName
+            viewHolder = ViewHolder(fruitImage, fruitName)
+            view.tag = viewHolder
+        }else{
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
         val fruit = getItem(position) // 获取当前项的Fruit实例
         if(fruit != null){
-            fruitImage.setImageResource(fruit.imageId)
-            fruitName.text = fruit.name
+            viewHolder.fruitImage.setImageResource(fruit.imageId)
+            viewHolder.fruitName.text = fruit.name
         }
         return view
     }
