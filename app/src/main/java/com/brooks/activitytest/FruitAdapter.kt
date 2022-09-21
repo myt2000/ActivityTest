01239ac39c29133db33f2c33c5ff8b386ff091c3
brooks <myt2000@126.com>
2022-09-21 10:29:52 +0800

    RecyclerView的点击事件

--- app/src/main/java/com/brooks/activitytest/FruitAdapter.kt
+++ app/src/main/java/com/brooks/activitytest/FruitAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fruit_item.view.*
 
@@ -15,7 +16,19 @@ class FruitAdapter(val fruitList: List<Fruit>): RecyclerView.Adapter<FruitAdapte
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.fruit_item,parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.fruit_item, parent, false)
+//        val viewHolder = ViewHolder(view)
+//        viewHolder.itemView.setOnClickListener {
+//            val position = viewHolder.adapterPosition
+//            val fruit = fruitList[position]
+//            Toast.makeText(parent.context, "you clicked view ${fruit.name}", Toast.LENGTH_SHORT).show()
+//        }
+//        // 这里fruitImage 必须写成小写
+//        viewHolder.fruitImage.setOnClickListener{
+//            val position = viewHolder.adapterPosition
+//            val fruit = fruitList[position]
+//            Toast.makeText(parent.context, "you clicked Image ${fruit.name}", Toast.LENGTH_SHORT).show()
+//        }
         return ViewHolder(view)
     }
 
@@ -23,6 +36,20 @@ class FruitAdapter(val fruitList: List<Fruit>): RecyclerView.Adapter<FruitAdapte
         val fruit = fruitList[position]
         holder.fruitImage.setImageResource(fruit.imageId)
         holder.fruitName.text = fruit.name
+
+        holder.itemView.setOnClickListener {
+            val position = holder.adapterPosition
+            val fruit = fruitList[position]
+            Toast.makeText(holder.itemView.context, "you clicked view ${fruit.name}", Toast.LENGTH_SHORT).show()
+        }
+
+        holder.fruitImage.setOnClickListener {
+            val position = holder.adapterPosition
+            val fruit = fruitList[position]
+            Toast.makeText(holder.itemView.context, "you clicked view ${fruit.name}", Toast.LENGTH_SHORT).show()
+        }
+        // 注：　onCreateViewHolde方法中的parent.context就相当于onBindViewHolder中的holer.itemView.context
+
     }
 
     override fun getItemCount() = fruitList.size
--- app/src/main/java/com/brooks/activitytest/MainActivity.kt
+++ app/src/main/java/com/brooks/activitytest/MainActivity.kt
@@ -2,7 +2,7 @@ package com.brooks.activitytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,22 +13,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initFruits()    // 初始化数据实例
-        val layoutManager = LinearLayoutManager(this)
-        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        val layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
         recyclerView.layoutManager = layoutManager
         val adapter = FruitAdapter(fruitList)
         recyclerView.adapter = adapter
     }
 
     private fun initFruits(){
-        repeat(3){
-            fruitList.add(Fruit("Apple", R.drawable.apple))
-            fruitList.add(Fruit("Banana", R.drawable.banana))
-            fruitList.add(Fruit("Orange", R.drawable.orange))
-            fruitList.add(Fruit("WaterMelon", R.drawable.watermelon))
-            fruitList.add(Fruit("Grape", R.drawable.grape))
-            fruitList.add(Fruit("Pineapple", R.drawable.pineapple))
-            fruitList.add(Fruit("Strawberry", R.drawable.strawberry))
+        repeat(5){
+            fruitList.add(Fruit(getRandomLengthString("Apple"), R.drawable.apple))
+            fruitList.add(Fruit(getRandomLengthString("Banana"), R.drawable.banana))
+            fruitList.add(Fruit(getRandomLengthString("Orange"), R.drawable.orange))
+            fruitList.add(Fruit(getRandomLengthString("WaterMelon"), R.drawable.watermelon))
+            fruitList.add(Fruit(getRandomLengthString("Grape"), R.drawable.grape))
+            fruitList.add(Fruit(getRandomLengthString("Pineapple"), R.drawable.pineapple))
+            fruitList.add(Fruit(getRandomLengthString("Strawberry"), R.drawable.strawberry))
         }
     }
+
+    private fun getRandomLengthString(str: String): String{
+        val n = (1..20).random()
+        val builder = StringBuilder()
+        repeat(n){
+            builder.append(str)
+        }
+        return builder.toString()
+    }
+
 }
